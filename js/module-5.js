2b25aff94a6bb55146696f2090217bd369e36d1d
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-02-18 16:48:20 +0200

    filter

--- js/module-5.js
+++ js/module-5.js
@@ -845,3 +845,355 @@ const getUserEmails = (users) => users.map(huuser => huuser.email);
 console.log(getUserEmails(users));
 
 console.log('Ти використовуєш users як параметр колбек-функції всередині map. Це може спричинити плутанину, оскільки параметр функції вже називається users. Більш коректний варіант:huuser - довільна назва, аби леше не збігалась з властивістю масиву');
+console.log('//============================================================//');
+// Методи filter і find
+// Метод filter()
+console.log('//====================Методи filter і find=================//');
+
+console.log('//=====================filter()==================//');
+
+// Метод filter(callback) використовується для єдиної операції — фільтрації масиву. Під фільтрацією масиву мається на увазі відбір усіх елементів з колекції за певним критерієм.
+
+
+
+// array.filter((element, index, array) => {
+//   // Тіло колбек-функції
+// });
+
+
+
+// Не змінює оригінальний масив.
+// Поелементно перебирає оригінальний масив.
+// Повертає новий масив.
+// Додає в масив, що повертається, елементи, які задовольняють умову колбек-функції.
+// Якщо колбек повернув true, елемент додається в масив, що повертається.
+// Якщо колбек повернув false, елемент не додається в масив, що повертається.
+// Якщо жоден елемент не задовольнив умову, повертає порожній масив.
+
+
+// const values = [51, -3, 27, 21, -68, 42, -37];
+
+// const positiveValues = values.filter(value => value >= 0);
+// console.log(positiveValues); // [51, 27, 21, 42]
+// // до positiveValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були >= 0
+
+// const negativeValues = values.filter(value => value < 0);
+// console.log(negativeValues); // [-3, -68, -37]
+// // до negativeValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були < 0
+
+// const bigValues = values.filter(value => value > 1000);
+// console.log(bigValues); // []
+// // до bigValues потрапили всі елементи масиву values, які задовольнили умову колбека, тобто були > 1000
+
+// console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+// // Оригінальний масив values не змінився
+
+
+
+// Отже, метод filter викликає колбек-функцію для кожного елемента вихідного масиву. Якщо результат її виконання true, поточний елемент додається в новий масив.
+
+console.log('//====================================================//');
+
+console.log('autocheck');
+
+// Доповни код так, щоб у змінній evenNumbers був масив парних чисел із масиву numbers, а у змінній oddNumbers — масив непарних. Обов'язково використовуй метод filter().
+
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+const oddNumbers = numbers.filter(number => number % 2 !==0);
+
+console.log(numbers); // original array
+console.log(evenNumbers); // new array with even numbers // numbers.filter(number => number % 2 === 0);
+console.log(oddNumbers); // new array with add numbers // numbers.filter(number => number % 2 !==0);
+
+console.log('//=====================================================//');
+
+console.log('Метод filter() на масиві об`єктів');
+
+// Метод filter() на масиві об'єктів
+
+
+
+// Під час роботи з масивом об'єктів виконується фільтрація за значенням певної властивості. У підсумку утворюється новий масив відфільтрованих об'єктів.
+
+
+
+// Розгляньмо приклад, де є масив студентів з балами за тест. Необхідно відфільтрувати студентів, які мають:
+
+
+
+// високі бали (від 80 (включно)),
+// низькі бали (нижче 50),
+// середні бали (від 50 (включно) до 80).
+
+
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+  { name: "Houston", score: 64 },
+];
+
+const best = students.filter(student => student.score >= HIGH_SCORE);
+console.log(best); // Масив об'єктів з іменами Mango і Kiwi
+
+const worst = students.filter(student => student.score < LOW_SCORE);
+console.log(worst); // Масив з одним об'єктом Ajax
+
+const average = students.filter(
+  (student) => student.score >= LOW_SCORE && student.score < HIGH_SCORE
+);
+console.log(average); // Масив об'єктів з іменами Poly і Houston
+
+// У прикладі вище метод filter() створює новий масив із тими об'єктами з масиву students, які задовольняють умови, встановлені на основі властивості score. Умови визначаються в колбек-функції, яка передається у filter() як аргумент.
+
+// У змінну best відфільтровано об'єкти, в яких значення властивості score більше або дорівнюєHIGH_SCORE. Результатом буде масив об'єктів з іменами "Mango" і "Kiwi".
+
+// У змінну worst — об’єкти, в яких значення властивості score менше LOW_SCORE. Результатом буде масив з одним об'єктом з іменем "Ajax".
+
+
+
+// У змінну average — об'єкти, в яких значення властивості score більше або дорівнює LOW_SCORE і менше HIGH_SCORE. Результатом буде масив об'єктів з іменами "Poly" і "Houston".
+
+console.log('//====================================================//');
+
+console.log('autocheck');
+
+// Масив books містить колекцію об'єктів книг, кожен з яких містить властивості title, author, rating. Використовуючи метод filter(), доповни код таким чином, щоб:
+
+// У змінній topRatedBooks утворився масив книг, рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
+// У змінній booksByAuthor утворився масив книг, написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
+// Оголошена змінна books
+// Значення змінної books - це масив об'єктів
+// Оголошена змінна MIN_RATING
+// Значення змінної MIN_RATING - це число 8
+// Оголошена змінна AUTHOR
+// Значення змінної AUTHOR - це рядок "Bernard Cornwell"
+// Оголошена змінна topRatedBooks
+// Значення змінної topRatedBooks - це масив книг з рейтингом, вищим за 8
+// Оголошена змінна booksByAuthor
+// Значення змінної booksByAuthor - це масив книг, автор яких "Bernard Cornwell"
+// Для перебирання масиву books використаний метод filter()
+
+const books19 = [       
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
+
+const topRatedBooks = books19.filter(book => book.rating >= MIN_RATING);
+const booksByAuthor = books19.filter(book => book.author === AUTHOR);
+
+console.log(topRatedBooks);
+console.log(booksByAuthor);
+
+console.log('//====================================================//');
+
+console.log('//====================================================//');
+
+console.log('autocheck');
+
+// Доповни функцію getUsersWithEyeColor(users, color) таким чином, щоб вона повертала масив користувачів, у яких колір очей (властивість eyeColor) збігається зі значенням другого параметра color.
+
+// Оголошена змінна getUsersWithEyeColor
+// Змінній getUsersWithEyeColor присвоєна стрілочна функція з параметрами (users, color)
+// Для перебирання параметра users використовується метод filter()
+// Якщо значення параметра color - це "blue", функція повертає масив об'єктів користувачів з іменами Moore Hensley, Sharlene Bush і Carey Barr
+// Якщо значення параметра color - це "green", функція повертає масив об'єктів користувачів з іменами Ross Vazquez і Elma Head
+// Якщо значення параметра color - це "brown", функція повертає масив об'єктів користувачів з іменами Blackburn Dotson і Sheree Anthony
+// Якщо значення параметра color - це будь-який інший рядок, функція повертає порожній масив
+
+
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+
+console.log(getUsersWithEyeColor(users, "blue"));
+console.log(getUsersWithEyeColor(users, "green"));
+console.log(getUsersWithEyeColor(users, "black"));
+
+console.log('//================================================//');
+
+console.log('//====================================================//');
+
+console.log('autocheck');
+
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику функції із завдання.
+
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+
+// Доповни код функції getUsersWithAge(users, minAge, maxAge), щоб повернути масив користувачів, вік (збережений у властивості age) яких потрапляє у заданий діапазон від minAge до maxAge.
+
+// Поради:
+
+// Використай метод filter() для створення нового масиву, в якому зберігаються тільки елементи, які задовольняють певну умову.
+// Використай оператори >= (більше або дорівнює) та <= (менше або дорівнює), щоб відфільтрувати користувачів, вік яких точно потрапляє у діапазон між мінімальним minAge та максимальним maxAge значеннями.
+// Оголошена змінна getUsersWithAge
+// Змінній getUsersWithAge присвоєна стрілочна функція з параметрами (users, minAge, maxAge)
+// Для перебирання параметра users використовується метод filter()
+// Якщо значення параметрів minAge і maxAge дорівнюють 20 і 30 відповідно, функція повертає масив об'єктів користувачів з іменами Ross Vazquez, Elma Head і Carey Barr
+// Якщо значення параметрів minAge і maxAge дорівнюють 30 і 40 відповідно, функція повертає масив об'єктів користувачів з іменами Moore Hensley, Sharlene Bush, Blackburn Dotson, Sheree Anthony
+// Якщо значення параметрів minAge і maxAge дорівнюють 80 і 100 відповідно, функція повертає порожній масив
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+
+const getUsersWithAge = (users, minAge, maxAge) => users.filter(user => user.age >= minAge && user.age <= maxAge);
+
+console.log(getUsersWithAge(users, 20, 30)); // [{…}, {…}, {…}]
+
