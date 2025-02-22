10790ca3b2d255e0be6295db21cc02ae797b86ee
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-02-22 18:07:07 +0200

    qwqccq

---
+++ js/module-6.js
@@ -0,0 +1,9 @@
+const user = {
+  username: "Victor",
+  showName() {
+		// ✅ Віктор біжить швидко, тому що він (this) намагається зловити поїзд.
+    console.log(this.username);
+  },
+};
+
+user.showName();
